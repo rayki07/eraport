d6807a60f7f2837165dd725dc8b0522a85292adf
rayki07 <firmanwahyudialakmal@gmail.com>
2026-02-20 05:28:43 +0700

    meredirect '/' ke dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+
+Route::get('/', function () {
+    return redirect()->route('dashboard');
 });
 
-Route::get('/home', function () {
+/* Route::get('/home', function () {
     return view('home');
-});
+}); */
 
 Route::get('/dashboard', function () {
     return view('dashboard');
